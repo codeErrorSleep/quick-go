581be8056ee72d10a1efa5d85d62d38300c50347
wenhaoqiu <wenhaoqiu@xiaoe-tech.com>
2022-01-26 17:00:56 +0800

    日志组件

--- bootstrap/conf/conf.go
+++ bootstrap/conf/conf.go
@@ -1,7 +1,15 @@
 package conf
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
 
 var (
 	Env *viper.Viper
+
+	ErrorLogger *zap.Logger
+	DebugLogger *zap.Logger
+	InfoLogger  *zap.Logger
+	CallLogger  *zap.Logger
 )
