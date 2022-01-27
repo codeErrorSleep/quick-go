e7a867b8c9147f871f8901c3a3a83d3df74764a2
wenhaoqiu <wenhaoqiu@xiaoe-tech.com>
2022-01-27 12:04:49 +0800

    mysql,redis连接

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"quick-go/conf"
+	"quick-go/db"
 	"quick-go/log"
 )
 
 func main() {
 	// 1.读取配置相关的信息
-	err := conf.RegisterConfig()
+	err := conf.InitConfig()
 	if err != nil {
 		print(err.Error())
 	}
 
 	// 2.日志系统
-	err = log.RegisterLog()
+	err = log.InitLog()
+	if err != nil {
+		print(err.Error())
+	}
+
+	// 3.初始化mysql
+	err = db.InitMysql()
+	if err != nil {
+		print(err.Error())
+	}
+
+	// 4.初始化redis
+	err = db.InitRedis()
 	if err != nil {
 		print(err.Error())
 	}
