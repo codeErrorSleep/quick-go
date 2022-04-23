9951d2c89b15cb3e0e224b30885ee744a04541de
wenhaoqiu <wenhaoqiu@xiaoe-tech.com>
2022-04-23 13:30:48 +0800

    新的需求修改

--- README.md
+++ README.md
@@ -6,13 +6,15 @@ go web 快速上手开发的脚手架
 gin+gorm
 
 # todo
-- sw...接口文档
 - dockerfile支持
 - grpc
 - 本地缓存
+- -go1.18升级
+- 中间件缓存
 
 
 # done
 - 开启的资源guanbi
 - kafka测试
 - gin合成
+- 中间件日志
--- test/route_test/stock_test.go
+++ test/route_test/stock_test.go
@@ -1,1 +1,23 @@
-package route_test
+package test
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	r *gin.Engine
+)
+
+
+func TestGetSpuStock(t *testing.T){
+	// 发起请求
+	w := request(r, "GET", "/", nil)
+
+	// 检查结果
+	assert.Equal(t, 200, w.Code)
+}
+
+
+
